fd50748d94cd892b4715ab1429dab3df10fd4601
Nickalaus Painter <paintena14@gmail.com>
2024-11-07 09:47:51 -0500

    Added more robust testing, fixed some whitespace in sorting.py, and added basic ArrayList without regard for potential off by 1 errors in translation.

--- testing.py
+++ testing.py
@@ -7,18 +7,29 @@ A = [x for x in range(N)]
 shuffle(A)
 
 def test_sort(func):
-    N = 10
+    N = 20
     A = [x for x in range(N)]
     shuffle(A)
-    print(A)
+    print("Sorting with " + str(func.__name__))
+    print("Before: " + str(A))
     func(A,N)
-    print(A)
+    print("After: " + str(A))
+    if not verify_sorted(A):
+        print("Algorithm failed!")
 
-test_sort(InsertionSort)
-test_sort(SelectionSort)
+def verify_sorted(A):
+    for i in range(0,len(A)-1):
+        if A[i] > A[i+1]:
+            return False
+    return True
+
+# wrapper functions to allow for easier testing
 def msort(A,n):
     MergeSort(A,0,n-1)
 def qsort(A,n):
     QuickSort(A,0,n-1)
+
+test_sort(InsertionSort)
+test_sort(SelectionSort)
 test_sort(msort)
 test_sort(qsort)
